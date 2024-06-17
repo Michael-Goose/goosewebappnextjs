032b30311aeb6d27b9aaf0c786ca296bee8909c7
Michael-Goose <michael@gooseinsurance.com>
2024-06-17 16:20:12 -0400

    icons and pages

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -39,25 +39,20 @@ export default function Page() {
       </div>
       <nav className="fixed inset-x-0 bottom-0 bg-white py-2">
         <div className="flex justify-around">
-          <a href="#" className="flex flex-col items-center text-xs">
+          <a href="home" className="flex flex-col items-center text-xs">
             <HomeIcon className="h-6 w-6 text-[#ff7eb9]" />
-            <span className="text-[#ff7eb9]">Home</span>
           </a>
-          <a href="#" className="flex flex-col items-center text-xs">
+          <a href="claims" className="flex flex-col items-center text-xs">
             <FileQuestionIcon className="h-6 w-6" />
-            <span>Claims</span>
           </a>
-          <a href="#" className="flex flex-col items-center text-xs">
+          <a href="my-policies" className="flex flex-col items-center text-xs">
             <FlagIcon className="h-6 w-6" />
-            <span>My Policies</span>
           </a>
-          <a href="#" className="flex flex-col items-center text-xs">
+          <a href="contact" className="flex flex-col items-center text-xs">
             <ContactIcon className="h-6 w-6" />
-            <span>Contact</span>
           </a>
-          <a href="#" className="flex flex-col items-center text-xs">
+          <a href="profile" className="flex flex-col items-center text-xs">
             <UserIcon className="h-6 w-6" />
-            <span>Profile</span>
           </a>
         </div>
       </nav>
@@ -143,21 +138,24 @@ function FlagIcon(props: IconProps) {
 
 function HomeIcon(props: IconProps) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="pink"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-      <polyline points="9 22 9 12 15 12 15 22" />
-    </svg>
+    <div className="text-center">
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="pink"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <polyline points="9 22 9 12 15 12 15 22" />
+      </svg>
+      <div className="text-pink-500">Home</div>
+    </div>
   );
 }
 
